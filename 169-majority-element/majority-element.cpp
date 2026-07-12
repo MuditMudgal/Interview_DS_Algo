fd6ba4da77d83e49fadf70290cee3104be163d9d
Mudit_Mudgxl <146031145+MuditMudgal@users.noreply.github.com>
2026-07-12 23:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (33.88%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,12 +5,12 @@ public:
         int cd = nums[0];
         int count = 1;
         for(int i=1;i<n;i++){
-            if(nums[i]==cd) count++;
-            if(nums[i]!=cd) count--;
             if(count==0){
                 cd = nums[i];
                 count++;
             }
+            else if(nums[i]==cd) count++;
+            else count--;
         }
         return cd;
     }
