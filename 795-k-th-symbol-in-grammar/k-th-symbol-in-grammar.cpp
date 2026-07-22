dc90ef785196fa7d5bb3b26807b4e767ac4423c6
Mudit_Mudgxl <146031145+MuditMudgal@users.noreply.github.com>
2026-07-22 13:20:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.12%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int kthGrammar(int n, int k) {
         if(n==1) return 0;
-        if(k%2==0){ //even->flip, k/2
-            int prevAns = kthGrammar(n-1,k/2);
-            if(prevAns==0) return 1;
+        if(k%2==0){
+            int a = kthGrammar(n-1,k/2);
+            if(a==0) return 1;
             else return 0;
         }
-        else{  //odd->same, (k/2)+1
-            int prevAns = kthGrammar(n-1,(k/2)+1);
-            return prevAns;
-        }
+        else  return kthGrammar(n-1,(k/2)+1);
+    
     }
 };
